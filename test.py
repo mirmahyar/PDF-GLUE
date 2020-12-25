a228fd096d1efb7352ca1a4ea217dccf898333fc
mirmahyar <m.mirsamadi@gmail.com>
2020-12-25 20:26:15 +0330

    deleted test

--- test.py
+++
@@ -1,7 +0,0 @@
-import PySimpleGUI as sg
-sg.theme("DarkTeal2")
-layout = [[sg.T("")], [sg.Text("Choose a file: "), sg.Input(), sg.FileBrowse()]]
-
-###Building Window
-window = sg.Window('My File Browser', layout, size=(600,150))
-event, values = window.read()

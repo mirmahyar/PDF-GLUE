71597cb01536028b259b624246648a675dff7577
mirmahyar <m.mirsamadi@gmail.com>
2020-12-25 20:54:56 +0330

    added browse destination folder to put output file

--- gi.py
+++ gi.py
@@ -1,4 +1,6 @@
 import PySimpleGUI as sg
+import PyPDF2
+import sys
 
 
 sg.theme('Light Blue 3')
@@ -6,6 +8,7 @@ layout = [
             [sg.Text("Welcome to Pdf Glue. Select your files below...")],
           [sg.Text("Choose first pdf file: "), sg.Input(key="-IN-"), sg.FileBrowse()],
           [sg.Text("Choose second pdf file: "), sg.Input(key="-IN-0"), sg.FileBrowse()],
+          [sg.Text("Choose destination folder: "), sg.Input(key="-IN-1"), sg.FolderBrowse()],
           [sg.Button("Submit")]
           ]
 window=sg.Window('Pdf Glue',layout,size=(800,400))
@@ -17,5 +20,36 @@ while True:
     if event == sg.WIN_CLOSED or event=="Exit":
         break
     elif event == "Submit":
-        print(values["-IN-"])
-        print(values["-IN-0"])
+        file1=values["-IN-"]
+        file2=values["-IN-0"]
+        destination=values["-IN-1"]
+
+        merger = PyPDF2.PdfFileMerger()
+        p1 = PyPDF2.PdfFileReader(file1)
+        p2 = PyPDF2.PdfFileReader(file2)
+
+        merger.append(p1)
+        merger.append(p2)
+
+        merger.write(destination + "/output.pdf") 
+                
+
+
+
+
+
+'''
+# Has the user entered any pdf files as arguments?
+if len(sys.argv) > 1:
+
+    p1 = PyPDF2.PdfFileReader(sys.argv[1])
+    p2 = PyPDF2.PdfFileReader(sys.argv[2])
+
+    merger.append(p1)
+    merger.append(p2)
+
+    merger.write("./output.pdf") '''
+
+
+
+
